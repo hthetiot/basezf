c91cdfc8300d41dd76729b86fc4c382941e0fdbd
hthetiot <harold@gmail.com>
2009-01-05 18:19:51 +0100

    add mootools to lib.js packed file and remove some useless /

--- etc/static/js/lib.js
+++ etc/static/js/lib.js
@@ -1,3 +1,7 @@
+# Mootools 1.2.1
+/js/lib/mootools-1.2.1-core.js
+/js/lib/mootools-1.2-more.js
+
 # Mootools Plugins files
 /js/lib/motools-plugins/Autocompleter/Autocompleter.js
 /js/lib/motools-plugins/Autocompleter/Autocompleter.Request.js
