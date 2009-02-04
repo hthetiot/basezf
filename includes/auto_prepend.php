c2d07f06044a655ed90e0ee32735764aeedc8490
Harold Thetiot <hthetiot@gmail.com>
2009-02-04 17:12:24 +0100

    set MAIN_URL as default value ['HTTP_HOST'] in auto_prepend

--- includes/auto_prepend.php
+++ includes/auto_prepend.php
@@ -82,7 +82,7 @@ define('CONFIG_STATIC_PACK_JS_PATH',  '/js/pack/');
 // External variable env
 
 // main url
-define_if_not('MAIN_URL', 'example.com');
+define_if_not('MAIN_URL', $_SERVER['HTTP_HOST']);
 define_if_not('BASE_URL', BASE_HTTP_SCHEME . MAIN_URL);
 
 // cdn url use BASE_HTTP_SCHEME to be secure too
