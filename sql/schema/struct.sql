843f34be9ad97f882ad8d2301062aa75e2b76c92
Harold Thetiot <hthetiot@gmail.com>
2009-07-16 13:54:29 +0200

    update sql/schema/struct.sql file

--- sql/schema/struct.sql
+++ sql/schema/struct.sql
@@ -9,33 +9,33 @@
 
 CREATE TABLE example
 (
-    example_id      SERIAL NOT NULL, -- use serial in place of mysql auto_increment
+    example_id      SERIAL NOT NULL UNIQUE, -- use serial in place of mysql auto_increment
     example_type_id INT NOT NULL DEFAULT 1,
-    unique          TEXT NOT NULL,
+    unique_string   TEXT NOT NULL,
     string          TEXT NOT NULL,
     state           INT2 NOT NULL DEFAULT 1,
     date            TIMESTAMP,
     creation        TIMESTAMP NOT NULL DEFAULT NOW()
-)
+);
 
 -- Comment all fiels, and with possible value if needed
-COMMENT ON COLUMN example.state IS 'Current state: 0 pending, 1 validate, -1 refuse';
+-- COMMENT ON COLUMN example.state IS 'Current state: 0 pending, 1 validate, -1 refuse';
 
--- dont miss the foreign keys
-ALTER TABLE ONLY example ADD CONSTRAINT example__example_type_id__fkey
-FOREIGN KEY (example_type_id) REFERENCES example_type(example_type_id);
+
 
 -- the unique index of curse
-CREATE UNIQUE INDEX example_unique_idx ON example USING btree(unique);
+CREATE UNIQUE INDEX example_unique_idx ON example USING btree(unique_string);
 
 -------------------------------------------------
 -- Example type possible values table
 
 CREATE TABLE example_type
 (
-    example_type_id INT NOT NULL DEFAULT nextval('example_type_id_seq'),
+    example_type_id SERIAL NOT NULL UNIQUE, -- use serial in place of mysql auto_increment
     label           TEXT NOT NULL,
     short_label     TEXT NOT NULL,
     creation        TIMESTAMP NOT NULL DEFAULT NOW()
 )
 
+-- dont miss the foreign keys
+--ALTER TABLE ONLY example ADD CONSTRAINT example__example_type_id__fkey FOREIGN KEY (example_type_id) REFERENCES example_type(example_type_id);
