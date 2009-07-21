c144c81314981aeda6f7b2608858842f836eddd4
Harold Thetiot <hthetiot@gmail.com>
2009-07-21 11:14:53 +0200

    add AjaxAbstract JS Class for BaseZF Helper

---
+++ public/js/lib/basezf/Helper/AjaxAbstract.js
@@ -0,0 +1,128 @@
+/**
+ * AjaxAbstract.js
+ *
+ * @category   BaseZF_JS_Helper
+ * @package    BaseZF
+ * @copyright  Copyright (c) 2008 BaseZF
+ * @author     Harold Thétiot (hthetiot)
+ */
+
+if (typeof BaseZF == "undefined") var BaseZF = {};
+if (typeof BaseZF.Helper == "undefined") BaseZF.Helper = {};
+
+BaseZF.Helper.AjaxAbstract = {
+
+    loadingElement: $empty,
+    initialized: false,
+    nbActiveRequest: 0,
+
+    Extends: BaseZF.Class.Helper,
+
+    initHelper: function() {
+
+        // get abtract layer
+        that = BaseZF.Helper.AjaxAbstract;
+
+        if (!$type($('loading')) || that.initialized) {
+            return;
+        }
+
+        // init loading div
+        that.loadingElement = $('loading');
+        that.loadingElement.setStyles({
+            'position': 'fixed',
+            'top': '0px',
+            'left': '0px'
+        });
+
+        that.initialized = true;
+        that.hideLoading(true);
+    },
+
+    hideLoading: function(force) {
+
+        // get abtract layer
+        that = BaseZF.Helper.AjaxAbstract;
+
+        that.nbActiveRequest--;
+
+        if (
+           that.loadingElement &&
+            (
+                $type(force) ||
+                (that.nbActiveRequest <= 0 && that.loadingElement.hasClass('loading'))
+            )
+        ) {
+
+            // hide loading
+            that.loadingElement.removeClass('loading');
+            that.loadingElement.fade(0);
+            that.nbActiveRequest = 0;
+        }
+    },
+
+    showLoading: function() {
+
+        // get abtract layer
+        that = BaseZF.Helper.AjaxAbstract;
+
+        that.nbActiveRequest++;
+
+        if (that.loadingElement && !that.loadingElement.hasClass('loading')) {
+            that.loadingElement.addClass('loading');
+            that.loadingElement.fade(1);
+        }
+    },
+
+    getRequest: function(options, type, origin, singleton) {
+
+        // one request per helper
+        if ($type(singleton) && $type(this.myRequest)) {
+            this.myRequest.cancel();
+        }
+
+        // set callback origin
+        if (typeof(origin) != 'undefined') {
+           requestCallback = this.requestCallback.bindWithEvent(origin)
+        } else {
+           requestCallback = this.requestCallback.bind(this)
+        }
+
+        options = $merge({
+            method: 'get',
+            evalScripts: false,
+            onCancel: this.hideLoading,
+            onFailure: this.hideLoading,
+            onRequest: this.showLoading,
+            onSuccess: requestCallback,
+            noCache: true
+        }
+        , options);
+
+        switch (type) {
+            case 'JSON':
+                this.myRequest = new Request.JSON(options);
+                break;
+
+            case 'HTML':
+                this.myRequest = new Request.HTML(options);
+                break;
+
+            default:
+                this.myRequest = new Request(options);
+                break;
+        }
+
+        return this.myRequest;
+    }
+    /*
+
+    // example callback for Request.HTML
+    requestCallback: function(responseTree, responseElements, responseHTML, responseJavaScript) {
+    }
+
+    // example callback for Request.JSON
+    requestCallback: function(json) {
+    }
+    */
+};
--- public/js/lib/basezf/Helper/AjaxLink.js
+++ public/js/lib/basezf/Helper/AjaxLink.js
@@ -10,127 +10,6 @@
 if (typeof BaseZF == "undefined") var BaseZF = {};
 if (typeof BaseZF.Helper == "undefined") BaseZF.Helper = {};
 
-
-BaseZF.Helper.AjaxAbstract = {
-
-    loadingElement: $empty,
-    initialized: false,
-    nbActiveRequest: 0,
-
-    Extends: BaseZF.Class.Helper,
-
-    initHelper: function() {
-
-        // get abtract layer
-        that = BaseZF.Helper.AjaxAbstract;
-
-        if (!$type($('loading')) || that.initialized) {
-            return;
-        }
-
-        // init loading div
-        that.loadingElement = $('loading');
-        that.loadingElement.setStyles({
-            'position': 'fixed',
-            'top': '0px',
-            'left': '0px'
-        });
-
-        that.initialized = true;
-        that.hideLoading(true);
-    },
-
-    hideLoading: function(force) {
-
-        // get abtract layer
-        that = BaseZF.Helper.AjaxAbstract;
-
-        that.nbActiveRequest--;
-
-        if (
-           that.loadingElement &&
-            (
-                $type(force) ||
-                (that.nbActiveRequest <= 0 && that.loadingElement.hasClass('loading'))
-            )
-        ) {
-
-            // hide loading
-            that.loadingElement.removeClass('loading');
-            that.loadingElement.fade(0);
-            that.nbActiveRequest = 0;
-        }
-    },
-
-    showLoading: function() {
-
-        // get abtract layer
-        that = BaseZF.Helper.AjaxAbstract;
-
-        that.nbActiveRequest++;
-
-        if (that.loadingElement && !that.loadingElement.hasClass('loading')) {
-            that.loadingElement.addClass('loading');
-            that.loadingElement.fade(1);
-        }
-    },
-
-    getRequest: function(options, type, origin, notUnique) {
-
-        // one request per helper
-        if (
-            typeof(notUnique) == 'undefined' &&
-            typeof(this.myRequest) != 'undefined'
-        ) {
-            this.myRequest.cancel();
-        }
-
-        // set callback origin
-        if (typeof(origin) != 'undefined') {
-           requestCallback = this.requestCallback.bindWithEvent(origin)
-        } else {
-           requestCallback = this.requestCallback.bind(this)
-        }
-
-        options = $merge({
-            method: 'get',
-            evalScripts: false,
-            onCancel: this.hideLoading,
-            onFailure: this.hideLoading,
-            onRequest: this.showLoading,
-            onSuccess: requestCallback,
-            noCache: true
-        }
-        , options);
-
-        switch (type) {
-            case 'JSON':
-                this.myRequest = new Request.JSON(options);
-                break;
-
-            case 'HTML':
-                this.myRequest = new Request.HTML(options);
-                break;
-
-            default:
-                this.myRequest = new Request(options);
-                break;
-        }
-
-        return this.myRequest;
-    }
-    /*
-
-    // example callback for Request.HTML
-    requestCallback: function(responseTree, responseElements, responseHTML, responseJavaScript) {
-    }
-
-    // example callback for Request.JSON
-    requestCallback: function(json) {
-    }
-    */
-};
-
 /**
  * AjaxLink Helper Class
  */
@@ -175,10 +54,6 @@ BaseZF.Helper.AjaxLink = new Class({
     },
 
     requestCallback: function(responseTree, responseElements, responseHTML, responseJavaScript) {
-
-        // get abtract layer
-        that = BaseZF.Helper.AjaxAbstract;
-
         var originElement = ($type(this) == 'element' ? $(this) : null)
 
         try {
@@ -195,7 +70,7 @@ BaseZF.Helper.AjaxLink = new Class({
             throw e;
         }
 
-        that.hideLoading();
+        this.hideLoading();
     }
 });
 
