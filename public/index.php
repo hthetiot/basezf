b8dc765a92bcf9c221d5ed5603ed39d75f14244b
Harold Thetiot <hthetiot@gmail.com>
2009-07-29 23:28:08 +0200

    add unavailable html page for fatal error

--- public/index.php
+++ public/index.php
@@ -48,24 +48,7 @@ try {
     header("Status: 503 Service Temporarily Unavailable");
     header("Retry-After: 120");
     header("Connection: Close");
-
-echo <<<EOD
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html>
-<head>
-    <title>503 Service Temporarily Unavailable</title>
-</head>
-<body>
-    <h1>Service Temporarily Unavailable</h1>
-    <p>
-        The server is temporarily unable to service your
-        request due to maintenance downtime or capacity
-        problems. Please try again later.
-    </p>
-</body>
-</html>
-EOD;
-
+    echo file_get_contents('./unavailable.html');
     echo ob_get_clean();
 
     // Exit with error status
