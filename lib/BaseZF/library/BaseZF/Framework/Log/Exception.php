75f95122c6768e6444ad0b5f6951768f33f5772e
Harold Thetiot <hthetiot@gmail.com>
2009-09-10 16:49:33 +0200

    Implement BaseZF_Framework_Log::Factory has Zend_Log factory

---
+++ lib/BaseZF/library/BaseZF/Framework/Log/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Exception class in /BazeZF/Framework/Log
+ *
+ * @category   BazeZF
+ * @package    BazeZF_Framework
+ * @copyright  Copyright (c) 2008 BazeZF
+ * @author     Harold Thetiot (hthetiot)
+ */
+
+class BazeZF_Framework_Log_Exception extends Zend_Log_Exception
+{
+
+}
