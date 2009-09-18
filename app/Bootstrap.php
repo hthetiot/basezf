0d6fbcebe12a7fb46a8599e642dae1f1cefb9289
Harold Thetiot <hthetiot@gmail.com>
2009-09-18 13:37:58 +0200

    clean app/Bootstrap.php

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -29,42 +29,6 @@ final class Bootstrap extends BaseZF_Framework_Application_Bootstrap
         $registry->setCurrentLocale();
     }
 
-    protected function _initZFDebug()
-    {
-        return;
-
-        $autoloader = Zend_Loader_Autoloader::getInstance();
-        $autoloader->registerNamespace('ZFDebug');
-
-        $registry = MyProject_Registry::getInstance();
-        $db = $registry->registry('db');
-        $dbCache = $registry->registry('cache');
-
-        $options = array(
-            'plugins' => array(
-                'Variables',
-                'Database'  => array('adapter' => $db),
-                'File'      => array(
-                    'base_path' => BASE_PATH,
-                    'library'   => array(
-                        'BaseZF'
-                    ),
-                ),
-                'Memory',
-                'Time',
-                'Registry',
-                'Cache'     => array('backend' => $dbCache->getBackend()),
-                'Exception'
-            ),
-        );
-        $debug = new ZFDebug_Controller_Plugin_Debug($options);
-
-        $this->bootstrap('frontController');
-        $frontController = $this->getResource('frontController');
-        $frontController->registerPlugin($debug);
-
-    }
-
     /**
      * Get available routes
      */
